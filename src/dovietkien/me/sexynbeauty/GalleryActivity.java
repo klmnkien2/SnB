69150c94b0a5efe7a8f1ca5cbac04cf655fcbba6
Kiendv <klmnkien2@gmail.com>
2014-03-23 23:45:23 +0700

    update

--- src/dovietkien/me/sexynbeauty/GalleryActivity.java
+++ src/dovietkien/me/sexynbeauty/GalleryActivity.java
@@ -20,6 +20,8 @@ import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 
 import dovietkien.me.sexynbeauty.model.Gag;
@@ -62,10 +64,11 @@ import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class GalleryActivity extends ActionBarActivity implements GagsController.ChangeListener {
-    private ProgressDialog dialog;
+public class GalleryActivity extends ActionBarActivity implements GalleryController.GalleryChangeListener {
+
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     MenuAdapter mListAdapter;
@@ -74,12 +77,8 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
     private AdView adView;
     private InterstitialAd interstitial;
     
-    private GagsController gagsController;
     private ImageLoader imageLoader;
     private String urlToLoad;
-    
-    // Url of viewing image
-    private Gag currentViewGag;
 
     // For load menu link, name
     private final String[] nav_names = {
@@ -99,13 +98,13 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
         
-        initImageLoader();
+        initImageLoader(); 
         addAds();
         urlToLoad = GagsParser.VN_URL;
 
-        gagsController = new GagsController(this);
+        mGalleryController = new GalleryController(this);
 
-        setupNavigator(savedInstanceState);
+        addTest();
     }
     
     private GalleryController mGalleryController;
@@ -133,7 +132,7 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
         LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         
         for(int i=0; i<4; i++) {
-            View view = inflater.inflate(R.layout.gallery_item, null);
+            final View view = inflater.inflate(R.layout.gallery_item, null);
             TextView mTitle = (TextView) view.findViewById(R.id._galleryItemTitle);        
             mTitle.setText("wat de fuk");        
             
@@ -155,124 +154,33 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
             default:
                 break;
             }
-            ImageView mImageView = (ImageView) view.findViewById(R.id._galleryItemImage);
-            if(url != null) imageLoader.displayImage(url, mImageView);
+            final ImageView mImageView = (ImageView) view.findViewById(R.id._galleryItemImage);
+            if(url != null) imageLoader.displayImage(url, mImageView, new ImageLoadingListener() {
+
+                @Override
+                public void onLoadingStarted(String arg0, View arg1) {
+                    
+                }
+
+                @Override
+                public void onLoadingFailed(String arg0, View arg1, FailReason arg2) {
+                   
+                }
+
+                @Override
+                public void onLoadingComplete(String arg0, View arg1, Bitmap arg2) {
+                    mImageView.getLayoutParams().height = 200;
+                }
+
+                @Override
+                public void onLoadingCancelled(String arg0, View arg1) {        
+                    
+                }
+            });
             
             if(i % 2 == 0) leftColView.addView(view);
             else rightColView.addView(view);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.gag, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        
-        // Handle action buttons
-        switch(item.getItemId()) {
-        case R.id.action_refesh:
-            refeshGags();  
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_refesh).setVisible(!drawerOpen);
-        getSupportActionBar().setTitle(drawerOpen?"Select":nav_names[mDrawerList.getCheckedItemPosition()]);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    private void setupNavigator(Bundle savedInstanceState) {
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
-        // set up the drawer's list view with items using a custom adapter and click listener
-        mListAdapter = new MenuAdapter(this, nav_names);
-        mDrawerList.setAdapter(mListAdapter);
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this, mDrawerLayout, R.drawable.overflow, R.string.drawer_open, R.string.drawer_close
-        ) {
-            public void onDrawerClosed(View view) {
-                supportInvalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            public void onDrawerOpened(View drawerView) {                
-                supportInvalidateOptionsMenu(); 
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-
-        if (savedInstanceState == null) {
-            selectItem(0);
-        }
-    }
-    
-    public void openNavigator(View v)
-    {
-        mDrawerLayout.openDrawer(Gravity.LEFT);
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    public void selectItem(int position) {
-        urlToLoad = nav_links[position];   
-        getMoreGags();
-        
-        mDrawerList.setItemChecked(position, true);
-        getSupportActionBar().setTitle(nav_names[position]);
-        mDrawerLayout.closeDrawer(mDrawerList);
-    }
-    
-    public void setCurrentViewGag(Gag gag) {
-        this.currentViewGag = gag;
-    }
-    
-    public void download(View v) {
-        new GagsDownloader(this).execute(currentViewGag.getImageUrl());
     }
     
     @Override
@@ -358,31 +266,6 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
         imageLoader = ImageLoader.getInstance();
         imageLoader.init(config);
     }
-
-    @Override
-    public void onPreLoading() {
-    }
-
-    @Override
-    public void onGag(List<Gag> gags, String nextUrl) { 
-        urlToLoad = nextUrl;      
-    }
-
-    @Override
-    public void onException(Exception ex) {
-    }
-
-    public void getGags(View v) {
-        gagsController.getGags(urlToLoad);
-    }
-
-    public void refeshGags() {
-        getMoreGags();
-    }
-    
-    public void getMoreGags() {
-        gagsController.getGags(urlToLoad);
-    }
     
     public void displayAlert(String title, String message) {
 
@@ -399,5 +282,21 @@ public class GalleryActivity extends ActionBarActivity implements GagsController
         });
 
         confirm.show().show();
+    }
+
+    @Override
+    public void onThumbsPreLoading() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void onThumbsLoaded(List<GalleryItem> thumbImageUrls, String nextUrl) {
+        urlToLoad = nextUrl;    
+    }
+
+    @Override
+    public void onThumbsLoadFail(Exception ex) {
+        
     }
 }
